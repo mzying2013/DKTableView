10abe34e8b67fa55351bf7f0bccb70508a52b902
bill <liumin2011@gmail.com>
2017-11-06 09:23:02 +0800

    Initial Commit

---
+++ DKTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DKTableView
+//
+//  Created by bill on 2017/11/6.
+//  Copyright © 2017年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
